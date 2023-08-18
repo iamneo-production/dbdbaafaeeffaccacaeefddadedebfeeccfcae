178da8687d284e17d20e61a6a9d9b8957a69c3b5
Iamneo <techadmin@iamneo.ai>
2023-08-18 13:07:10 +0000

    Backup Commit

--- Project/src/test/java/utils/Screenshot.java
+++ Project/src/test/java/utils/Screenshot.java
@@ -1,79 +1,37 @@
-// package utils;
-
-// import java.io.*;
-// import java.text.SimpleDateFormat;
-// import java.util.Date;
-// import java.util.TimeZone;
-
-// import org.openqa.selenium.OutputType;
-// import org.openqa.selenium.TakesScreenshot;
-// import org.openqa.selenium.WebDriver;
-
-// public class Screenshot {
-//     public String captureScreenshot(WebDriver driver, String screenshotName) {
-
-//         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-
-//         TimeZone istTimeZone = TimeZone.getTimeZone("Asia/Kolkata"); // IST timezone
-
-//         dateFormat.setTimeZone(istTimeZone);
-
-//         String timestamp = dateFormat.format(new Date());
-
-//         // Define the screenshots directory path
-//         String screenshotsDirectory = System.getProperty("user.dir") + "/src/main/screenshots/";
-
-//         // Create the screenshots directory if it doesn't exist
-//         File directory = new File(screenshotsDirectory);
-
-//         if (!directory.exists()) {
-//             directory.mkdirs();
-//         }
-
-//         File srcScreenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-//         String destinationScreenshotPath = screenshotsDirectory + screenshotName + "_" + timestamp + ".png";
-
-//         try {
-//             // Copy the screenshot file using Files.copy
-//             java.nio.file.Files.copy(srcScreenshot.toPath(), new File(destinationScreenshotPath).toPath());
-//         } catch (IOException e) {
-//             e.printStackTrace();
-//         }
-
-//         return destinationScreenshotPath;
-//     }
-// }
-
 package utils;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
-
 public class Screenshot {
     public String captureScreenshot(WebDriver driver, String screenshotName) {
+
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+
         TimeZone istTimeZone = TimeZone.getTimeZone("Asia/Kolkata"); // IST timezone
+
         dateFormat.setTimeZone(istTimeZone);
+
         String timestamp = dateFormat.format(new Date());
 
         // Define the screenshots directory path
-        String screenshotsDirectory = "src/main/Screenshot/";
+        String screenshotsDirectory = System.getProperty("user.dir") + "/src/main/screenshots/";
 
         // Create the screenshots directory if it doesn't exist
         File directory = new File(screenshotsDirectory);
+
         if (!directory.exists()) {
             directory.mkdirs();
         }
 
         File srcScreenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        String destinationScreenshotPath = "/Project/"+screenshotsDirectory + screenshotName + "_" + timestamp + ".png";
+        String destinationScreenshotPath = screenshotsDirectory + screenshotName + "_" + timestamp + ".png";
 
         try {
             // Copy the screenshot file using Files.copy
@@ -85,3 +43,45 @@ public class Screenshot {
         return destinationScreenshotPath;
     }
 }
+
+// package utils;
+
+// import org.openqa.selenium.OutputType;
+// import org.openqa.selenium.TakesScreenshot;
+// import org.openqa.selenium.WebDriver;
+
+// import java.io.File;
+// import java.io.IOException;
+// import java.text.SimpleDateFormat;
+// import java.util.Date;
+// import java.util.TimeZone;
+
+// public class Screenshot {
+//     public String captureScreenshot(WebDriver driver, String screenshotName) {
+//         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+//         TimeZone istTimeZone = TimeZone.getTimeZone("Asia/Kolkata"); // IST timezone
+//         dateFormat.setTimeZone(istTimeZone);
+//         String timestamp = dateFormat.format(new Date());
+
+//         // Define the screenshots directory path
+//         String screenshotsDirectory = "src/main/Screenshot/";
+
+//         // Create the screenshots directory if it doesn't exist
+//         File directory = new File(screenshotsDirectory);
+//         if (!directory.exists()) {
+//             directory.mkdirs();
+//         }
+
+//         File srcScreenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+//         String destinationScreenshotPath = "/Project/"+screenshotsDirectory + screenshotName + "_" + timestamp + ".png";
+
+//         try {
+//             // Copy the screenshot file using Files.copy
+//             java.nio.file.Files.copy(srcScreenshot.toPath(), new File(destinationScreenshotPath).toPath());
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+
+//         return destinationScreenshotPath;
+//     }
+// }
